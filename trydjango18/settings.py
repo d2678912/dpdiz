f75b7ad90070ab410a90eb4755efba42e745a552
root <root@dpdiz.com>
2018-03-27 10:00:52 +0000

    test dpdiz

--- trydjango18/settings.py
+++ trydjango18/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'trydjango18.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': 'qwerty',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
